c67f601829b40ac5756fb324c05a90b1d0ec54cd
diandrameilaa <dianndraaa00@gmail.com>
2024-12-06 17:24:27 +0700

    done

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,5 +1,5 @@
 <header class="main-nav">
-    <div class="sidebar-user text-center">
+    <div class="sidebar-user text-center"><a class="setting-primary" href="javascript:void(0)"><i data-feather="settings"></i></a><img class="img-90 rounded-circle" src="../assets/images/dashboard/1.png" alt="">
         <h6 class="mt-3 f-14 f-w-600">{{ Auth::user()->name }}</h6>
         <p class="mb-0 font-roboto">
             @if(Auth::user()->role === 'admin')
--- routes/web.php
+++ routes/web.php
@@ -32,38 +32,54 @@ Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('regi
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Reports
-Route::get('/reports', [ReportController::class, 'index'])->name('reports.index');
-Route::get('/reports/data', [ReportController::class, 'getData'])->name('reports.data');
-Route::delete('/reports/{id}', [ReportController::class, 'destroy'])->name('reports.destroy');
-Route::get('/reports/create', [ReportController::class, 'create'])->name('reports.create');
-Route::post('/reports', [ReportController::class, 'store'])->name('reports.store');
-Route::get('reports/{id}/edit', [ReportController::class, 'edit'])->name('reports.edit');
-Route::put('reports/{id}', [ReportController::class, 'update'])->name('reports.update');
 
-// Notifications
-Route::resource('notifications', NotificationController::class);
 
-// Routes for projects
-Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
-Route::get('/projects/data', [ProjectController::class, 'getData'])->name('projects.data');
-Route::delete('/projects/{id}', [ProjectController::class, 'destroy'])->name('projects.destroy');
-Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
-Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
-Route::get('/projects/{id}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
-Route::put('/projects/{id}', [ProjectController::class, 'update'])->name('projects.update');
 
- // Routes for user
- Route::get('/user', [UserController::class, 'index'])->name('user.index');
- Route::get('/user/data', [UserController::class, 'getData'])->name('user.data');
- Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
- Route::post('/user', [UserController::class, 'store'])->name('user.store');
- Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
- Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
- Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
- Route::post('/user/status', [UserController::class, 'toggleStatus'])->name('user.toggle_status');
+ Route::middleware(['auth', 'admin'])->group(function () {
+    // Admin routes, accessible only to authenticated admins
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+    Route::get('/user/data', [UserController::class, 'getData'])->name('user.data');
+    Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+    Route::post('/user', [UserController::class, 'store'])->name('user.store');
+    Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
+    Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
+    Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+    Route::post('/user/status', [UserController::class, 'toggleStatus'])->name('user.toggle_status');
 
- // Discussions
- Route::get('/discussions', [DiscussionController::class, 'index'])->name('discussions.index');
- Route::post('/discussions', [DiscussionController::class, 'store'])->name('discussions.store');
- Route::delete('/discussions/{id}', [DiscussionController::class, 'destroy'])->name('discussions.destroy');
+    // Routes for projects
+    Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+    Route::get('/projects/data', [ProjectController::class, 'getData'])->name('projects.data');
+    Route::delete('/projects/{id}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+    Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
+    Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
+    Route::get('/projects/{id}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+    Route::put('/projects/{id}', [ProjectController::class, 'update'])->name('projects.update');
+
+
+    // Notifications
+    Route::resource('notifications', NotificationController::class);
+
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    // Reports
+    Route::get('/reports', [ReportController::class, 'index'])->name('reports.index');
+    Route::get('/reports/data', [ReportController::class, 'getData'])->name('reports.data');
+    Route::delete('/reports/{id}', [ReportController::class, 'destroy'])->name('reports.destroy');
+    Route::get('/reports/create', [ReportController::class, 'create'])->name('reports.create');
+    Route::post('/reports', [ReportController::class, 'store'])->name('reports.store');
+    Route::get('reports/{id}/edit', [ReportController::class, 'edit'])->name('reports.edit');
+    Route::put('reports/{id}', [ReportController::class, 'update'])->name('reports.update');
+
+    // Projects
+    Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+    Route::get('/projects/data', [ProjectController::class, 'getData'])->name('projects.data');
+
+    // Discussions
+    Route::get('/discussions', [DiscussionController::class, 'index'])->name('discussions.index');
+    Route::post('/discussions', [DiscussionController::class, 'store'])->name('discussions.store');
+    Route::delete('/discussions/{id}', [DiscussionController::class, 'destroy'])->name('discussions.destroy');
+});
